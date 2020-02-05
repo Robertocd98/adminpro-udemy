598931297a6eefb84730735e876f293591ac8780
Roberto Carbajales <robertocd98@gmail.com>
2020-02-05 09:58:15 -0400

    input, out, viewchild

---
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html',
+  styleUrls: ['./grafico-dona.component.css']
+})
+export class GraficoDonaComponent implements OnInit {
+
+  @Input() public doughnutChartLabels: Label[] = [];
+  @Input() public doughnutChartData: MultiDataSet = [];
+  @Input() public doughnutChartType: ChartType ;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 // Modulos
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
 // Rutas
 import { PAGES_ROUTES } from './pages.routes';
+
+// ng2-chars
+import { ChartsModule } from 'ng2-charts';
 
 import { PagesComponent } from './pages.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
@@ -20,6 +29,8 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -29,7 +40,9 @@ import { Graficas1Component } from './graficas1/graficas1.component';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 
 })
